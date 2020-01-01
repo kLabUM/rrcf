9536ddc753aa3112fd9f3fc9db2dd471eed86663
Matt Bartos <mdbartos@umich.edu>
2020-01-01 17:50:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='rrcf',
-      version='0.3.2',
+      version='0.3.3',
       description='Robust random cut forest for anomaly detection',
       author='Matt Bartos, Abhiram Mullapudi, Sara Troutman',
       author_email='mdbartos@umich.edu, abhiramm@umich.edu, stroutm@umich.edu',
